312d2df09d7b6ffa8aa425f4e386d79f114afb32
부상훈 <>
2024-08-04 12:04:56 +0900

    add gitignore & update gradle version

--- spring-httpclient/src/main/java/com/sh8121/springhttpclient/archive/httpserver/App.java
+++ spring-httpclient/src/main/java/com/sh8121/springhttpclient/archive/httpserver/App.java
@@ -2,11 +2,16 @@ package com.sh8121.springhttpclient.archive.httpserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+//        SpringApplication.run(App.class, args);
+
+        Flux.just(1,2,3,4)
+                .log()
+                .map(i -> i * 2);
     }
 }
